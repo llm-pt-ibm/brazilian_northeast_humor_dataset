94ce99fc7e57ede9a71857d71a58fd05a3376d55
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-05-15 08:22:42 -0300

    add a function to organize the final dataset

--- database/main.py
+++ database/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from database_creation.yt_data_extractor import *
+from yt_data_extractor import *
 from yt_video_transcriber import *
 
 def extract_audios_from_youtube_videos():
@@ -11,13 +11,30 @@ def extract_audios_from_youtube_videos():
             yt_data_extractor.extract_audio_from_youtube_video(video_url, 'data/audios')
             print('Step completed.')
         except Exception as e:
-            print(e)
-            print(f'Step failed for url {video_url}.')
-
-extract_audios_from_youtube_videos()
+            print(f'Step failed for url {video_url}.\n{e}')
 
 def extract_transcripts_from_youtube_videos():
     yt_transcriber = YouTubeVideoTranscriber()
     yt_transcriber.transcribe_youtube_videos()
 
+def organize_final_dataset():
+    annotated_df = pd.read_csv('data/annotated/data_with_new_annotation_from_peer_review_comments.csv')
+
+    videos_df = pd.read_csv('data/transcriptions/youtube_videos_transcribed_with_metadata.csv')
+    videos_df = videos_df[['video_url', 'video_title', 'publish_date']]
+
+    final_df = pd.merge(annotated_df, videos_df, on='video_url', how='left')
+    final_df = final_df[new_columns_order]
+
+    new_columns_order = ['video_url', 'video_title', 'publish_date', 'brazilian_state', 'text_origin',
+        'corrected_transcription', 'specific_contexts', 'punchlines',
+       'fun', 'humor', 'nonsense', 'wit', 'irony', 'satire', 'sarcasm',
+       'cynicism', 'joke_explanation']
+    final_df = final_df[new_columns_order]
+
+    os.makedirs('data/completed', exist_ok=True)
+    final_df.to_csv('data/completed/brazilian_ne_annotated_humorous_texts.csv', index=False)
+
+extract_audios_from_youtube_videos()
 extract_transcripts_from_youtube_videos()
+#organize_final_dataset()
